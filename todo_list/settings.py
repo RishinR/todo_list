680dc05d2c0adec55b496c789fc45a32073c3ec1
RishinR <rishinrnew2020@gmail.com>
2024-05-07 23:54:56 +0530

    commiting to render

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--(fvg0yfrkt#*o_f@idogf9o@x06@*u1-_aja@t1(zaw4_c$u_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://todo-list-t2i3.onrender.com']
+ALLOWED_HOSTS = ['todo-list-t2i3.onrender.com']
 
 
 # Application definition
